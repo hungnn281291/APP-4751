850b86cce2e12ddd876d5129f7f876acb081cf6c
Hung Ngoc Nguyen <116861287+hungnn281291@users.noreply.github.com>
2023-03-11 16:15:16 +1100

    Update app-insights.tf

--- app-insights.tf
+++ app-insights.tf
@@ -1,5 +1,5 @@
 module "application_insights" {
-  source                   = "git::https://9025-CICD@dev.azure.com/9025-CICD/ESLZ%20Modules/_git/terraform-azurerm-application-insights?ref=1.0.1"
+  source                   = "git::https://github.com/hungnn281291/Modules.git/terraform-azurerm-application-insights"
   application_insight_name = module.naming.generated_names.domain.application_insights[0]
   resource_group_name      = data.azurerm_resource_group.nerdio.name
   location                 = data.azurerm_resource_group.nerdio.location
